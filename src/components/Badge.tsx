a8baad3e4b89dd930e9ce7017e6fd40a941075cb
Jamessdevops <142910595+Jamessdevops@users.noreply.github.com>
2026-01-17 19:23:23 +0530

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -32,6 +32,7 @@ export const Badge = ({
           <motion.path
             stroke="currentColor"
             strokeLinecap="round"
+            strokeLinecap="round"
             strokeWidth="1.5"
             d="M10.75 8.75L14.25 12L10.75 15.25"
             initial={{ pathLength: 0 }}
